eafcc5fd748518df717f5dedfcc3d7236166ebd4
Simon Blurton <sblurton@hotmail.co.uk>
2022-04-14 20:45:03 +0100

    new feature: useAxiosGet hook written.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
-import * as React from 'react';
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import useSWR, { KeyedMutator } from "swr";
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+const getFetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+type Status = "idle" | "pending" | "fulfilled" | "error";
+
+function useAxiosGet<Data>(
+  url: string
+): [
+  status: Status,
+  data: Data[] | undefined,
+  error: AxiosError | undefined,
+  mutate: KeyedMutator<Data[]>
+] {
+  const [status, setStatus] = useState<Status>("idle");
+  const { data, isValidating, mutate, error } = useSWR<Data[]>(url, getFetcher);
+
+  useEffect(() => {
+    if ((!data && !error) || isValidating) return setStatus("pending");
+    if (error) return setStatus("error");
+    if (data && !isValidating) return setStatus("fulfilled");
+  }, [data, status, isValidating, error]);
+
+  return [status, data, error, mutate];
 };
+
+export default useAxiosGet
