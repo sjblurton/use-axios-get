2d3127051e4055866c7c2b6a65e3184eceb68c71
Simon Blurton <sblurton@hotmail.co.uk>
2022-04-15 07:52:51 +0100

    refactor: standard refactor to get tsdx working.

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,29 @@
-import axios, { AxiosError } from "axios";
-import { useEffect, useState } from "react";
-import useSWR, { KeyedMutator } from "swr";
+import axios, { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
+import useSWR, { KeyedMutator } from 'swr';
 
-const getFetcher = (url: string) => axios.get(url).then((res) => res.data);
+const getFetcher = (url: string) => axios.get(url).then(res => res.data);
 
-type Status = "idle" | "pending" | "fulfilled" | "error";
+type Status = 'idle' | 'pending' | 'fulfilled' | 'error';
 
 function useAxiosGet<Data>(
   url: string
-): [
-  status: Status,
-  data: Data[] | undefined,
-  error: AxiosError | undefined,
-  mutate: KeyedMutator<Data[]>
-] {
-  const [status, setStatus] = useState<Status>("idle");
+): {
+  status: Status;
+  data: Data[] | undefined;
+  error: AxiosError | undefined;
+  mutate: KeyedMutator<Data[]>;
+} {
+  const [status, setStatus] = useState<Status>('idle');
   const { data, isValidating, mutate, error } = useSWR<Data[]>(url, getFetcher);
 
   useEffect(() => {
-    if ((!data && !error) || isValidating) return setStatus("pending");
-    if (error) return setStatus("error");
-    if (data && !isValidating) return setStatus("fulfilled");
+    if ((!data && !error) || isValidating) return setStatus('pending');
+    if (error) return setStatus('error');
+    if (data && !isValidating) return setStatus('fulfilled');
   }, [data, status, isValidating, error]);
 
-  return [status, data, error, mutate];
-};
+  return { status, data, error, mutate };
+}
 
-export default useAxiosGet
+export default useAxiosGet;
